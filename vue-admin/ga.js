7c1b94a3ccfd530b6b011a0761c8efd500ab8faf
lmxdawn@qq.com <lmxdawn@qq.com>
2018-04-28 18:01:36 +0800

    Google 的 analytics 统计

---
+++ vue-admin/ga.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Host-0034 on 2018/4/28.
+ */
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-110990780-1', 'auto');
+ga('send', 'pageview');
+
+window.addEventListener('hashchange', function () {
+    ga('set', 'page', window.location.href);
+    ga('send', 'pageview');
+});
+</script>
